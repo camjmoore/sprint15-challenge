68e76eccf5d8a7611cff6aa27e24c23cc7b98caf
cam <cjmooredev@gmail.com>
2024-02-23 17:57:02 -0400

    implemented restricted access middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,19 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+  const token = req.headers.authorization;
+  const secret = process.env.JWT_SECRET || 'is it secret? is it safe?';
 
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if(!err){
+        req.decodedToken = decodedToken;
+        next();
+      } else {
+        res.status(401).json({message: 'token invalid'});
+      }
+    })
+  } else {
+    res.status(401).json({ message: "token required" });
+  }
 };
