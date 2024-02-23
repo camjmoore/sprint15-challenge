5016f6ec2205ec66d7d2115a1cce4a3179e30a38
cam <cjmooredev@gmail.com>
2024-02-23 17:42:34 -0400

    corrected import paths and implemented login endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const Users = require('../user/user-model.js');
-const { validateUser } = require('../user/user-middleware.js');
+const validateUser = require('../middleware/user-validation.js');
+const generateToken = require('../middleware/generate-token.js');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
 router.post('/register', validateUser, (req, res) => {
 
@@ -25,29 +25,25 @@ router.post('/register', validateUser, (req, res) => {
 
 router.post('/login', (req, res) => {
   res.end('implement login, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
 
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).json({ message: 'username and password required' });
+  }
+
+  Users.getBy({ username })
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: `welcome, ${user.username}`, token });
+      } else {
+        res.status(401).json({ message: 'invalid credentials' });
       }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    })
 });
 
 module.exports = router;
