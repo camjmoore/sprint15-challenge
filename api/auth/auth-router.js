412b9ce600eed084f956fb297125b0a95b4bb672
cam <cjmooredev@gmail.com>
2024-02-25 19:51:31 -0400

    modified 400 and 500 responses with res.end() to exit process

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -8,7 +8,7 @@ router.post('/register', (req, res) => {
   const { username } = req.body;
 
   if (!userCreds.username || !userCreds.password) {
-    res.status(400).json({ message: 'username and password required' });
+    res.status(400).end('username and password required');
   }
 
   const ROUNDS = process.env.BCRYPT_ROUNDS || 8;
@@ -20,14 +20,14 @@ router.post('/register', (req, res) => {
   Users.getBy({username})
     .then(user => {
       if (user) {
-        res.status(400).json({ message: 'username taken' });
+        res.status(400).end('username taken');
       } else {
         Users.add(userCreds)
         .then(user => {
           res.status(201).json(user);
         })
         .catch(err => {
-          res.status(500).json({ message: 'fucked innit?' });
+          res.status(500).end();
         })
       }
     })
@@ -39,7 +39,7 @@ router.post('/login', (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    res.status(400).json({ message: 'username and password required' });
+    res.status(400).end('username and password required');
   }
 
   Users.getBy({ username })
@@ -48,7 +48,7 @@ router.post('/login', (req, res) => {
         const token = generateToken(user);
         res.status(200).json({ message: `welcome, ${user.username}`, token });
       } else {
-        res.status(401).json({ message: 'invalid credentials' });
+        res.status(401).end('invalid credentials');
       }
     })
     .catch(err => {
